e0f7fdc990b43f18dbc0773c2b75cd3f86ca8797
Wang Qiu <winsonwq@gmail.com>
2015-11-26 10:59:34 +0800

    add faskclick

--- src/client.js
+++ src/client.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import FastClick from 'fastclick';
 import App from './components/app.react';
 
 React.initializeTouchEvents(true);
+FastClick(document.body);
 global.loadjs([global.viewName], function(component) {
   React.render(
     <App view={ component } props={ global.props } viewName={ global.viewName } />,
